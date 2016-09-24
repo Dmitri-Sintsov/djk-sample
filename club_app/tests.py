7da7c24c1a3bde231b4276a53b11d64e106ceea4
Dmitriy Sintsov <sdvpartnership@mail.ru>
2016-09-24 11:46:07 +0300

    Added simple selenium test.

--- club_app/tests.py
+++ club_app/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-# Create your tests here.
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+from django_jinja_knockout.tpl import reverseq
+
+
+class SeleniumMixin:
+    pass
+
+
+class ClubAppTests(StaticLiveServerTestCase):
+    # fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(20)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def get_reverse_url(self, viewname, kwargs=None, query=None):
+        url = '{}{}'.format(
+            self.live_server_url, reverseq(viewname=viewname, kwargs=kwargs, query=query)
+        )
+        print('get_reverse_url: {}'.format(url))
+        return self.selenium.get(url)
+
+    def test_signup(self):
+        from selenium.webdriver.support.wait import WebDriverWait
+        timeout = 20
+        self.get_reverse_url('account_signup')
+
+        username_input = self.selenium.find_element_by_id("id_username")
+        username_input.send_keys('testuser')
+        password_input = self.selenium.find_element_by_id("id_password1")
+        password_input.send_keys('test123')
+        password_input = self.selenium.find_element_by_id("id_password2")
+        password_input.send_keys('test123')
+
+        self.selenium.find_element_by_xpath('//input[@value="Sign Up »"]').click()
--- djk_sample/settings.py
+++ djk_sample/settings.py
@@ -162,6 +162,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': ':memory:',
+        },
     }
 }
 
