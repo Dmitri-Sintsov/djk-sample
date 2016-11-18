7e237c23b41491031c8228b3108d963da334bc59
Dmitriy Sintsov <questpc256@gmail.com>
2016-11-18 13:21:56 +0300

    Completed ClubAppTests.empty_club_list(), .add_sport_club() test in progress.

--- club_app/tests.py
+++ club_app/tests.py
@@ -3,6 +3,7 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 from django.utils.html import format_html
 
 from django_jinja_knockout.tpl import reverseq
+from django_jinja_knockout.viewmodels import to_json
 from django_jinja_knockout.testing import SeleniumCommands
 
 
@@ -16,13 +17,19 @@ class ClubAppTests(SeleniumCommands):
     def do_has_messages_success(self):
         return self.do_by_xpath('//div[@class="messages"]/div[@class="alert alert-danger success"]')
 
+    def do_jumbotron_text(self, text):
+        return self.do_by_xpath(
+            '//div[@class="jumbotron"]/div[@class="default-padding" and contains(text(), "{}")]'.format(text),
+        )
+
     def register_new_user(self):
         self.exec(
             'reverse_url', {'viewname': 'account_signup'},
-            'keys_by_id', ('id_username', 'testuser'),
+            'keys_by_id',
+                ('id_username', 'testuser'),
                 ('id_password1', 'test123'),
                 ('id_password2', 'test123'),
-            'find_submit_by_viewname', ('account_signup',),
+            'find_submit_by_view', ('account_signup',),
             'click',
             'has_messages_success',
         )
@@ -30,7 +37,7 @@ class ClubAppTests(SeleniumCommands):
     def logout_user(self):
         self.exec(
             'reverse_url', {'viewname': 'account_logout'},
-            'find_submit_by_viewname', ('account_logout',),
+            'find_submit_by_view', ('account_logout',),
             'click',
             'has_messages_success',
         )
@@ -38,9 +45,10 @@ class ClubAppTests(SeleniumCommands):
     def login_user(self):
         self.exec(
             'reverse_url', {'viewname': 'account_login'},
-            'keys_by_id', ('id_login', 'testuser'),
-            ('id_password', 'test123'),
-            'find_submit_by_viewname', ('account_login',),
+            'keys_by_id',
+                ('id_login', 'testuser'),
+                ('id_password', 'test123'),
+            'find_submit_by_view', ('account_login',),
             'click',
             'has_messages_success',
         )
@@ -48,14 +56,39 @@ class ClubAppTests(SeleniumCommands):
     def empty_club_list(self):
         self.exec(
             'reverse_url', {'viewname': 'club_list'},
-            'by_xpath', (
-                '//div[@class="jumbotron"]/div[@class="default-padding" and contains(text(), "There is")]',
-            )
+            'jumbotron_text', ('There is no',),
+            'find_anchor_by_view', (
+                'club_list',
+                {},
+                {
+                    'list_filter': to_json({
+                        'category': 1
+                    })
+                }
+            ),
+            'click',
+            'jumbotron_text', ('There is no',),
+            'reverse_url', (
+                'club_list',
+                {},
+                {
+                    'list_filter': to_json({
+                        'category1': 1
+                    })
+                }
+            ),
+            'jumbotron_text', ('Not allowed filter field',),
         )
 
     def add_sport_club(self):
         self.exec(
-            'reverse_url', {'viewname': 'club_create'}
+            'reverse_url', {'viewname': 'club_create'},
+            'keys_by_id',
+                ('id_title', 'Yaroslavl Bears'),
+                ('id_foundation_date', '1971-08-29'),
+            'by_id', ('id_category_1',),
+            'element_by_xpath', ('parent::*',),
+            'click',
         )
 
     def test_signup(self):
@@ -65,4 +98,4 @@ class ClubAppTests(SeleniumCommands):
         self.logout_user()
         self.login_user()
         self.empty_club_list()
-        # self.add_sport_club()
+        self.add_sport_club()
