ef5c4dfd5bddeb4357e4f9c01dbb2ee14241cf06
Dmitriy Sintsov <sdvpartnership@mail.ru>
2016-11-20 19:04:41 +0300

    ClubAppTests.add_sport_club() in progress.

--- club_app/tests.py
+++ club_app/tests.py
@@ -4,57 +4,15 @@ from django.utils.html import format_html
 
 from django_jinja_knockout.tpl import reverseq
 from django_jinja_knockout.viewmodels import to_json
-from django_jinja_knockout.testing import SeleniumCommands
+from django_jinja_knockout.testing import DjkSeleniumCommands
 
 
-class ClubAppTests(SeleniumCommands):
+class ClubAppTests(DjkSeleniumCommands):
     # fixtures = ['user-data.json']
 
     @classmethod
     def selenium_factory(cls):
         return WebDriver()
-
-    def do_has_messages_success(self):
-        return self.do_by_xpath('//div[@class="messages"]/div[@class="alert alert-danger success"]')
-
-    def do_jumbotron_text(self, text):
-        return self.do_by_xpath(
-            '//div[@class="jumbotron"]/div[@class="default-padding" and contains(text(), "{}")]'.format(text),
-        )
-
-    def do_input_as_select_click(self, id):
-        self.exec(
-            'by_id', (id,),
-            'element_by_xpath', ('parent::label',),
-            'click',
-        )
-        return self.last_result
-
-    def do_fk_widget_click(self, id):
-        self.exec(
-            'by_id', (id,),
-            'element_by_xpath', ('following-sibling::button',),
-            'click',
-            'to_active_element',
-        )
-        return self.last_result
-
-    def do_grid_dialog_button_action_click(self, action_name):
-        self.exec(
-            'by_classname', ('bootstrap-dialog-message',),
-            'element_by_xpath', ('//span[text()="{}"]/parent::button'.format(action_name),),
-            'click',
-        )
-        return self.last_result
-
-    def do_dialog_button_click(self, button_title):
-        self.exec(
-            'to_active_element',
-            'element_by_xpath',
-                ('//div[@class="bootstrap-dialog-footer"]//button[contains(., "{}")]'.format(button_title),),
-            'click'
-        )
-        return self.last_result
 
     def register_new_user(self):
         self.exec(
@@ -122,15 +80,22 @@ class ClubAppTests(SeleniumCommands):
                 ('id_foundation_date', '1971-08-29'),
             'input_as_select_click', ('id_category_1',),
             'fk_widget_click', ('id_equipment_set-0-manufacturer',),
-            'grid_dialog_button_action_click', ('Add',),
+            'grid_button_action_click', ('Add',),
             'dialog_button_click', ('Save',),
+            'assert_field_error', ('id_company_name', 'This field is required.'),
+            'keys_by_id', ('id_company_name', 'Yanex'),
+            'by_id', ('id_direct_shipping',),
+            'click',
+            'dialog_button_click', ('Save',),
+        )
+        self.exec(
+            'to_active_element',
+            'grid_find_data_column', ('Company name', 'Yanex'),
         )
 
     def test_signup(self):
-        from selenium.webdriver.support.wait import WebDriverWait
-        timeout = self.__class__.WAIT_SECONDS
         self.register_new_user()
-        self.logout_user()
-        self.login_user()
-        self.empty_club_list()
+        # self.logout_user()
+        # self.login_user()
+        # self.empty_club_list()
         self.add_sport_club()
