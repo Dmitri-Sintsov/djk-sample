b8adef8088a0c5a77ce6192bc0c9fa2436693e45
Dmitriy Sintsov <questpc256@gmail.com>
2016-11-28 12:54:35 +0300

    Introduced ClubAppTests.AddSportClub / .UpdateSportClub to make selenium testing more modular.

--- club_app/tests.py
+++ club_app/tests.py
@@ -69,15 +69,19 @@ class ClubAppTests(DjkSeleniumCommands):
             'jumbotron_text', ('Not allowed filter field',),
         )
 
-    def add_sport_club(self):
-        self.exec(
+    class AddSportClub:
+
+        club_yaroslavl_bears_base_info = (
             'click_anchor_by_view', {'viewname': 'club_create'},
             # Next one step is optional:
             'form_by_view', ('club_create',),
             'keys_by_id',
-                ('id_title', 'Yaroslavl Bears'),
-                ('id_foundation_date', '1971-08-29'),
+            ('id_title', 'Yaroslavl Bears'),
+            ('id_foundation_date', '1971-08-29'),
             'input_as_select_click', ('id_category_1',),
+        )
+
+        add_manufacturer_yanex = (
             'fk_widget_add_and_select', (
                 'id_equipment_set-0-manufacturer',
                 (
@@ -93,28 +97,40 @@ class ClubAppTests(DjkSeleniumCommands):
             ),
             'keys_by_id', ('id_equipment_set-0-inventory_name', 'Silent Air 2020'),
             'input_as_select_click', ('id_equipment_set-0-category_2',),
+        )
+
+        add_member_ivan_petrov = (
             'relative_form_button_click', ('Add "Sport club member"',),
             'fk_widget_add_and_select', (
                 'id_member_set-0-profile',
                 (
                     'keys_by_id',
-                        ('id_first_name', 'Ivan',),
-                        ('id_last_name', 'Petrov',),
-                        ('id_birth_date', '1971-07-29',),
+                    ('id_first_name', 'Ivan',),
+                    ('id_last_name', 'Petrov',),
+                    ('id_birth_date', '1971-07-29',),
                 ),
                 (
                     'grid_find_data_column', ('First name', 'Ivan'),
                 )
             ),
             'keys_by_id',
-                ('id_member_set-0-last_visit', '2016-11-23 14:47:33'),
-                ('id_member_set-0-note', 'Veteran of Russian badminton'),
+            ('id_member_set-0-last_visit', '2016-11-23 14:47:33'),
+            ('id_member_set-0-note', 'Veteran of Russian badminton'),
             'input_as_select_click', ('id_member_set-0-plays_0',),
             'input_as_select_click', ('id_member_set-0-role_1',),
             'by_id', ('id_member_set-0-is_endorsed',),
             'click',
-            'click_submit_by_view', ('club_create',),
         )
+
+        @classmethod
+        def get_commands(cls):
+            commands = (
+                cls.club_yaroslavl_bears_base_info +
+                cls.add_manufacturer_yanex +
+                cls.add_member_ivan_petrov +
+                ('click_submit_by_view', ('club_create',),)
+            )
+            return commands
 
     def details_sport_club(self):
         self.exec(
@@ -122,17 +138,29 @@ class ClubAppTests(DjkSeleniumCommands):
             'dialog_button_click', ('OK',),
         )
 
-    def edit_sport_club(self):
+    class UpdateSportClub:
+
         update_view = {'viewname': 'club_update', 'kwargs': {'club_id': 1}}
-        self.exec(
-            'click_anchor_by_view', update_view,
-            'form_by_view', update_view,
-            # 'relative_form_button_click', ('Add "Sport club equipment"',),
-            # 'by_id', ('id_equipment_set-1-DELETE',),
-            # 'click',
-            # 'to_top_bootstrap_dialog',
-            # 'dialog_button_click', ('Yes',),
-            'form_by_view', update_view,
+
+        @classmethod
+        def open_update_form(cls):
+            return (
+                'click_anchor_by_view', cls.update_view,
+                'form_by_view', cls.update_view,
+            )
+
+        @classmethod
+        def test_formset_removal(cls):
+            return (
+                'relative_form_button_click', ('Add "Sport club equipment"',),
+                'by_id', ('id_equipment_set-1-DELETE',),
+                'click',
+                'to_top_bootstrap_dialog',
+                'dialog_button_click', ('Yes',),
+                'form_by_view', cls.update_view,
+            )
+
+        add_equipment_bubblelat = (
             'relative_form_button_click', ('Add "Sport club equipment"',),
             'fk_widget_add_and_select', (
                 'id_equipment_set-1-manufacturer',
@@ -145,6 +173,9 @@ class ClubAppTests(DjkSeleniumCommands):
             ),
             'keys_by_id', ('id_equipment_set-1-inventory_name', 'Bubble Pro 2010'),
             'input_as_select_click', ('id_equipment_set-1-category_1',),
+        )
+
+        add_member_john_smith = (
             'relative_form_button_click', ('Add "Sport club member"',),
             'fk_widget_add_and_select', (
                 'id_member_set-1-profile',
@@ -163,15 +194,25 @@ class ClubAppTests(DjkSeleniumCommands):
             ('id_member_set-1-note', 'Enterpreneur and master of squash'),
             'input_as_select_click', ('id_member_set-1-plays_3',),
             'input_as_select_click', ('id_member_set-1-role_0',),
-            'click_submit_by_view', update_view,
         )
+
+        @classmethod
+        def get_commands(cls):
+            commands = (
+                cls.open_update_form() +
+                # cls.test_formset_removal() +
+                cls.add_equipment_bubblelat +
+                cls.add_member_john_smith +
+                ('click_submit_by_view', cls.update_view,)
+            )
+            return commands
 
     def test_all(self):
         self.register_new_user()
-        # self.logout_user()
-        # self.login_user()
-        # self.empty_club_list()
-        self.add_sport_club()
+        self.logout_user()
+        self.login_user()
+        self.empty_club_list()
+        self.exec_class(self.__class__.AddSportClub)
         self.details_sport_club()
-        self.edit_sport_club()
+        self.exec_class(self.__class__.UpdateSportClub)
         self._sleep(3)
