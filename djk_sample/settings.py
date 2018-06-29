f90396749a39d4a44744ec29f890079217a3f458
Dmitriy Sintsov <questpc256@gmail.com>
2018-06-29 12:07:53 +0300

    Renamed ko-grid.js to grid.js.

--- djk_sample/settings.py
+++ djk_sample/settings.py
@@ -208,6 +208,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+VUE_INTERPOLATION = False
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
