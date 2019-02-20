dfe76577bc19efd857d81ba60cacd65f898fe668
Dmitriy Sintsov <questpc256@gmail.com>
2019-02-20 19:53:25 +0300

    Dropped compatibility to old-style middleware.

--- djk_sample/middleware.py
+++ djk_sample/middleware.py
@@ -6,12 +6,12 @@ class ContextMiddleware(BaseContextMiddleware):
     def add_action(self, obj, action_type):
         self.__class__.add_instance('actions', (obj, action_type))
 
-    def save_actions(self, request):
+    def save_actions(self):
         from event_app.models import Action
         for args in self.__class__.yield_out_instances('actions'):
             Action.do(*args)
 
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        result = super().process_view(request, view_func, view_args, view_kwargs)
-        self.save_actions(request)
+    def djk_view(self, view_func, view_args, view_kwargs):
+        result = super().djk_view(view_func, view_args, view_kwargs)
+        self.save_actions()
         return result
