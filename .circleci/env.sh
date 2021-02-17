bf63850aafce65c8fc83a3f32db9f50fc75a5347
Dmitriy Sintsov <questpc256@gmail.com>
2021-02-17 13:20:34 +0300

    Diagnostic output for Circle CI tests.

--- .circleci/env.sh
+++ .circleci/env.sh
@@ -8,7 +8,16 @@ pyenv init -
 echo Version of Python installed in the python:latest-browsers image.
 python3 --version
 # export PATH="$(pyenv root)/versions/3.9.0/bin/:$PATH"
-echo Version of Python used to run the tests.
+echo Environment used to run the tests
+echo Kernel version
+uname -a
+echo System version
+lsb_release -a
+echo Python version
 python3 --version
+echo Chrome version
+google-chrome --version
+echo Chrome driver version
+chromedriver --version
 # https://stackoverflow.com/questions/27849412/permissionerror-with-pip3
 python3 -m pip install -U pip wheel tox tox-pyenv setuptools pysqlite3 --user
