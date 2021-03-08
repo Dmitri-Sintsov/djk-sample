1e74a1e5df941fe3caa240099520072e3fa17c67
Dmitriy Sintsov <questpc256@gmail.com>
2021-03-08 21:49:55 +0300

    Django 3.2 compatibility.

---
+++ club_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ClubAppConfig(AppConfig):
+    name = 'club_app'
+    verbose_name = 'Sport clubs'
+    default_auto_field = 'django.db.models.BigAutoField'
---
+++ event_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EventAppConfig(AppConfig):
+    name = 'event_app'
+    verbose_name = 'Club events'
+    default_auto_field = 'django.db.models.BigAutoField'
