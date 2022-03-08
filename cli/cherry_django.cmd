9a1d209d68c6cf75a016f9e007757087381ad50f
Dmitriy Sintsov <questpc256@gmail.com>
2022-03-09 00:51:41 +0300

    Simplified Windows cherry_django.cmd / collectrollup.cmd scripts.

--- cli/cherry_django.cmd
+++ cli/cherry_django.cmd
@@ -1,23 +1,4 @@
-if defined DJANGO_DEBUG (
-set _DJANGO_DEBUG=%DJANGO_DEBUG%
-)
-set DJANGO_DEBUG='False'
-
-if defined CHERRYPY_STATIC (
-set _CHERRYPY_STATIC=%CHERRYPY_STATIC%
-)
-set CHERRYPY_STATIC='True'
+set DJANGO_DEBUG=False
+set CHERRYPY_STATIC=True
 
 python %VIRTUAL_ENV%/djk-sample/cherry_django.py
-
-if defined _DJANGO_DEBUG (
-set DJANGO_DEBUG=%_DJANGO_DEBUG%
-) else (
-set DJANGO_DEBUG=
-)
-
-if defined _CHERRYPY_STATIC (
-set CHERRYPY_STATIC=%_CHERRYPY_STATIC%
-) else (
-set CHERRYPY_STATIC=
-)
